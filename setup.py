1a4103a1acf40ddea08ecfff5b28fb69917ff257
Dhruv Maheshwari <dhruv@Dhruvs-MacBook-Pro.local>
2017-10-29 23:05:20 -0700

    changed setup

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
+REQUIREMENTS = [i.strip() for i in open("requirements.txt").readlines()]
+
 setup(
     name='NameAnalyzer',
     packages=['NameAnalyzer'],
     include_package_data=True,
-    install_requires=[
-        'flask',
-    ],
+    install_requires=REQUIREMENTS
 )
 
